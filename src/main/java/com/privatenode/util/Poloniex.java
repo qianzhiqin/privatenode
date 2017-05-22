4a31c054a4c264ed077e971b2c7f9ce24601ae0f
qianzhiqin <qianzhiqin@jd.com>
2017-05-22 16:08:15 +0800

    +poloniex

---
+++ src/main/java/com/privatenode/util/Poloniex.java
@@ -0,0 +1,77 @@
+package com.privatenode.util;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by qianzhiqin on 2017/5/22.
+ */
+public class Poloniex {
+
+    public static String getValue(String url) {
+        String result = "";
+        try {
+            Document doc = Jsoup.connect(url).ignoreContentType(true).userAgent("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; BIDUBrowser 2.x)")
+                    .timeout(30000).followRedirects(true).get();
+            Elements elements = doc.select("body");
+            String json = elements.get(0).childNode(0).toString();
+            System.out.println(elements.get(0).childNode(0));
+            result = parseJson(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+        }
+        return result;
+    }
+
+
+    public static String getUnixtime() {
+        long unixtime = System.currentTimeMillis() / 1000 - 1800;
+        System.out.println(unixtime);
+        return String.valueOf(unixtime);
+    }
+
+    public static String geturl(String coin, String unixtime) {
+        String url = "https://poloniex.com/public?command=returnChartData&currencyPair=USDT_" + coin + "&start=" + unixtime + "&end=9999999999&period=300";
+        System.out.println(url);
+        return url;
+    }
+
+    public static String parseJson(String json) {
+        Type type = new TypeToken<ArrayList<Map<String, Object>>>() {
+        }.getType();
+        Gson gson = new Gson();
+        ArrayList<Map<String, Object>> list = gson.fromJson(json, type);
+        Map<String, Object> map = list.get(list.size() - 1);
+        String val = map.get("weightedAverage").toString();
+        System.out.println(val);
+        return val;
+    }
+    public static Map<String,String> getPoloniex(){
+        Map<String,String> map =new HashMap<>();
+
+        String unixtime = getUnixtime();
+        String zec = geturl("ZEC", unixtime);
+        String etc = geturl("ETC", unixtime);
+        String eth = geturl("ETH", unixtime);
+        getValue(zec);
+        map.put("zec",getValue(zec));
+        map.put("etc",getValue(etc));
+        map.put("eth",getValue(eth));
+        System.out.println(map);
+        return map;
+    }
+
+    public static void main(String[] args) {
+       getPoloniex();
+    }
+}
